76a8c991525afb00294cf7af0e7854982f430326
Baicoianu Ioan-Sorin <71268639+BaicoianuIoanSorin@users.noreply.github.com>
2023-04-06 08:58:37 -0700

    camera & player animation
    Added script for player animation and added following camera on player

--- Romanian MazeRunner 2D/Assets/CameraController.cs
+++ Romanian MazeRunner 2D/Assets/CameraController.cs
@@ -21,10 +21,10 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
-        if (player.transform.localScale.x > 0f)
-        {
-            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
-        }
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        // if (player.transform.localScale.x > 0f)
+        // {
+        //     playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        // }
     }
 }
