63e7b067bf9590063b65a180433e19b7399b7931
Adi-Vaitis <adriangabriel344@gmail.com>
2023-04-23 15:10:22 +0200

    Refator

--- Romanian MazeRunner 2D/Assets/Scripts/Menu/EndMenuScript.cs
+++ Romanian MazeRunner 2D/Assets/Scripts/Menu/EndMenuScript.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndMenuScript : MonoBehaviour
 {
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void GoBackToStart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
